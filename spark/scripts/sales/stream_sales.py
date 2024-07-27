9b5c8ec4c7853e6c090593985d8204cd6cf7fd32
OskarMierkiewicz <84857371+OskarMierkiewicz@users.noreply.github.com>
2024-07-27 18:07:26 +0200

    DS-37, DS-45

--- spark/scripts/sales/stream_sales.py
+++ spark/scripts/sales/stream_sales.py
@@ -1,12 +1,11 @@
 from pyspark.sql import SparkSession, functions as F
-from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 #Spark session initialize
 spark = SparkSession \
     .builder \
-    .appName("KafkaToCassandra") \
-    .config("spark.cassandra.connection.host", "172.30.0.6") \
+    .appName("SalesDataStreaming") \
+    .config("spark.cassandra.connection.host", "172.30.0.9") \
     .config("spark.cassandra.connection.port","9042")\
     .config("spark.cassandra.auth.username","cassandra")\
     .config("spark.cassandra.auth.password","cassandra")\
@@ -15,25 +14,37 @@ spark = SparkSession \
 spark.sparkContext.setLogLevel("ERROR")
 
 #Data Schema
-flat_schema = StructType([
-    StructField("category", StringType(), nullable=False),
-    StructField("subcategories", StringType(), nullable=False),
-    StructField("product", StringType(), nullable=False),
-    StructField("id", IntegerType(), nullable=False),
-	StructField("quantity", IntegerType(), nullable=True),
-    StructField("timestamp", StringType(), nullable=True)
+sales_schema = StructType([
+    StructField("Customer ID", IntegerType(), nullable=False),
+    StructField("Age", IntegerType(), nullable=False),
+    StructField("Gender", StringType(), nullable=False),
+    StructField("Item Purchased", StringType(), nullable=False),
+    StructField("Category", StringType(), nullable=False),
+    StructField("Purchase Amount (USD)", DoubleType(), nullable=False),
+    StructField("Location", StringType(), nullable=False),
+    StructField("Size", StringType(), nullable=False),
+    StructField("Color", StringType(), nullable=False),
+    StructField("Season", StringType(), nullable=False),
+    StructField("Review Rating", DoubleType(), nullable=False),
+    StructField("Subscription Status", StringType(), nullable=False),
+    StructField("Shipping Type", StringType(), nullable=False),
+    StructField("Discount Applied", StringType(), nullable=False),
+    StructField("Promo Code Used", StringType(), nullable=False),
+    StructField("Previous Purchases", IntegerType(), nullable=False),
+    StructField("Payment Method", StringType(), nullable=False),
+    StructField("Frequency of Purchases", StringType(), nullable=False)
 ])
 
 kafka_df = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "172.30.0.5:9092") \
-    .option("subscribe", "products_data") \
+    .option("kafka.bootstrap.servers", "172.30.0.6:9092, 172.30.0.7:9092, 172.30.0.8:9092") \
+    .option("subscribe", "sales") \
     .option("startingOffsets", "earliest") \
     .load()
 
 value_df = kafka_df.selectExpr("CAST(value AS STRING)")
-parsed_df = value_df.withColumn("data", F.from_json(F.col("value"), flat_schema)).select("data.*")
+parsed_df = value_df.withColumn("data", F.from_json(F.col("value"), sales_schema)).select("data.*")
 
 #add column record_id as an UUID
 parsed_df = parsed_df.withColumn("record_id", F.expr("uuid()"))
@@ -46,8 +57,8 @@ console_query = parsed_df \
     .start()
 
 #Cassandra connect credentials
-keyspace = "products_data"
-table = "data"
+keyspace = "company_one"
+table = "sales_data"
 
 #save data into Cassandra
 query = parsed_df \
--- spark/scripts/warehouse/stream_warehouse.py
+++ spark/scripts/warehouse/stream_warehouse.py
@@ -1,12 +1,11 @@
 from pyspark.sql import SparkSession, functions as F
-from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 #Spark session initialize
 spark = SparkSession \
     .builder \
-    .appName("KafkaToCassandra") \
-    .config("spark.cassandra.connection.host", "172.30.0.6") \
+    .appName("WarehouseDataStreaming") \
+    .config("spark.cassandra.connection.host", "172.30.0.9") \
     .config("spark.cassandra.connection.port","9042")\
     .config("spark.cassandra.auth.username","cassandra")\
     .config("spark.cassandra.auth.password","cassandra")\
@@ -14,31 +13,47 @@ spark = SparkSession \
     .getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
-#Data Schema
-flat_schema = StructType([
-    StructField("category", StringType(), nullable=False),
-    StructField("subcategories", StringType(), nullable=False),
-    StructField("product", StringType(), nullable=False),
-    StructField("id", IntegerType(), nullable=False),
-	StructField("quantity", IntegerType(), nullable=True),
-    StructField("timestamp", StringType(), nullable=True)
+#Schema
+warehouse_schema = StructType([
+    StructField("Warehouse ID", IntegerType(), nullable=False),
+    StructField("Product ID", IntegerType(), nullable=False),
+    StructField("Product Name", StringType(), nullable=False),
+    StructField("Category", StringType(), nullable=False),
+    StructField("Quantity in Stock", DoubleType(), nullable=False),
+    StructField("Location", StringType(), nullable=False),
+    StructField("Supplier", StringType(), nullable=False),
+    StructField("Restock Frequency", StringType(), nullable=False),
+    StructField("Average Delivery Time (days)", DoubleType(), nullable=False),
+    StructField("Stock Value (USD)", DoubleType(), nullable=False),
+    StructField("Storage Temperature (C)", DoubleType(), nullable=False),
+    StructField("Shelf Life (days)", DoubleType(), nullable=False),
+    StructField("Hazardous Material", StringType(), nullable=False),
+    StructField("Reorder Level", DoubleType(), nullable=False),
+    StructField("Daily Sales (units)", DoubleType(), nullable=False),
+    StructField("Daily Restock (units)", DoubleType(), nullable=False),
+    StructField("Stock Level (units)", IntegerType(), nullable=False),
+    StructField("Sales Forecast (units)", IntegerType(), nullable=False),
+    StructField("Latitude", DoubleType(), nullable=False),
+    StructField("Longitude", DoubleType(), nullable=False),
+    StructField("State", StringType(), nullable=False),
+    StructField("timestamp", TimestampType(), nullable=True)
 ])
 
 kafka_df = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "172.30.0.5:9092") \
-    .option("subscribe", "products_data") \
+    .option("kafka.bootstrap.servers", "172.30.0.6:9092, 172.30.0.7:9092, 172.30.0.8:9092") \
+    .option("subscribe", "warehouse") \
     .option("startingOffsets", "earliest") \
     .load()
 
 value_df = kafka_df.selectExpr("CAST(value AS STRING)")
-parsed_df = value_df.withColumn("data", F.from_json(F.col("value"), flat_schema)).select("data.*")
+parsed_df = value_df.withColumn("data", F.from_json(F.col("value"), warehouse_schema)).select("data.*")
 
 #add column record_id as an UUID
 parsed_df = parsed_df.withColumn("record_id", F.expr("uuid()"))
 
-#print to console
+#print to console - as a check
 console_query = parsed_df \
     .writeStream \
     .format("console") \
@@ -46,8 +61,8 @@ console_query = parsed_df \
     .start()
 
 #Cassandra connect credentials
-keyspace = "products_data"
-table = "data"
+keyspace = "company_one"
+table = "warehouse_data"
 
 #save data into Cassandra
 query = parsed_df \
